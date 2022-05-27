204f3e0e65c3972e0b917c12819670c6834a5e84
aurellieandra <diandra.azra@gmail.com>
2022-05-27 19:36:21 +0700

    adding api to get data

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import { Card, Container } from 'react-bootstrap'
+import styles from './style.module.css'
 
-export default function Display() {
+import userIcon from '../../assets/cm-icon/fi_users.png'
+import settingsIcon from '../../assets/cm-icon/fi_settings.png'
+import calendarIcon from '../../assets/cm-icon/fi_calendar.png'
+
+export default function Display({ car }) {
     return (
-        <div>
-            Display
-        </div>
+        <Container className={styles.cm_container}>
+            <Card className={styles.cm_card}>
+                <div className={styles.cm_content}>
+                    <div className={styles.cm_up_content}>
+                        <img src={car.image} alt={car.manufacture} className={styles.cm_image} />
+                        <h5 className="mt-3">{car.model}</h5>
+                        <h4>Rp. {car.rentPerDay} / hari</h4>
+                        <p>{car.description}</p>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1">
+                            <img className={styles.cm_icon} src={userIcon} />
+                        </div>
+                        <div className="col">
+                            <p>{car.capacity} orang</p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1">
+                            <img className="cm-icon" src={settingsIcon} />
+                        </div>
+                        <div className="col">
+                            <p>{car.transmission}</p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1">
+                            <img className="cm-icon" src={calendarIcon} />
+                        </div>
+                        <div className="col">
+                            <p>{car.year}</p>
+                        </div>
+                    </div>
+
+                    <a href="#" className="btn btn-success text-white">Pilih Mobil</a>
+                </div>
+            </Card>
+        </Container>
     )
 }
--- src/pages/Cars/index.js
+++ src/pages/Cars/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
+import styles from './style.module.css'
 
 import Navbar from '../../components/navbar'
 import CarsHeader from '../../components/cars header'
@@ -6,13 +8,32 @@ import Filter from '../../components/filter'
 import Display from '../../components/display'
 import Footer from '../../components/footer'
 
+import getCarsService from '../../services/getCars'
+
 export default function Cars() {
+    const [cars, setCars] = useState([])
+
+    const getCars = async () => {
+        const allCars = await getCarsService()
+        setCars(allCars)
+    }
+
+    useEffect(() => {
+        getCars()
+    }, [])
+
     return (
         <div>
             <Navbar />
             <CarsHeader />
             <Filter />
-            <Display />
+            <Container>
+                <div className={styles.display}>
+                    {
+                        cars.map((item, index) => <Display key={index} car={item} />)
+                    }
+                </div>
+            </Container>
             <Footer />
         </div>
     )
